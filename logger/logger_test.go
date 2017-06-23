c4ae0c0580ff8748e4112d5c38efe33e64aa0657
Ciisco <EthanBB13@gmail.com>
2017-06-23 14:32:22 -0300

    Created test for logger package, some fixes required

---
+++ logger/logger_test.go
@@ -0,0 +1,24 @@
+package logger_test
+
+import (
+	"testing"
+	"github.com/EthanG78/tweego/logger"
+)
+
+var log logger.Newlogger
+
+func TestNewlogger_Critical(t *testing.T) {
+	log.Critical("This is a test")
+}
+
+func TestNewlogger_Criticalf(t *testing.T) {
+	log.Criticalf("This is a test, logger is of type %T", log)
+}
+
+func TestNewlogger_Notice(t *testing.T) {
+	log.Notice("This is a test")
+}
+
+func TestNewlogger_Noticef(t *testing.T) {
+	log.Noticef("This is a test, logger is of type %T", log)
+}
